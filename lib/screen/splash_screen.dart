ca6d2b780a015a077db79f722b72b5b99763f617
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-30 19:12:56 +0500

    splash screen build

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,15 +1,32 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:notepad/navigation/router.gr.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
+    Future.delayed(const Duration(seconds: 1), () {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user == null) {
+        context.router.replace(const LoginScreen());
+      } else {
+        context.router.replace(const NoteListScreen());
+      }
+    });
 
-    if (user == null) {}
-
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: ClipOval(
+          child: Image.asset(
+            "assets/images/icon.png",
+            width: 200,
+            height: 200,
+          ),
+        ),
+      ),
+    );
   }
 }
