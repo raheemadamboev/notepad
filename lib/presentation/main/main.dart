78ee1070fed9131de16d29b7ff6e743e45ecd489
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-31 13:46:11 +0500

    proper packaging

--- lib/presentation/main/main.dart
+++ lib/presentation/main/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notepad/presentation/navigation/router.gr.dart';
 
-import 'firebase_options.dart';
+import '../../data/remote/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
